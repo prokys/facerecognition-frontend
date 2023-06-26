3da68224783b9637e2d39a8adcdf467637f284b1
Prokop Ferdan <prokes1@gmail.com>
2023-06-26 11:49:37 +0200

    started react app and created components: Navigation, Logo, Rank, ImageLinkForm and added background with ParticlesBG

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg'
+
+const config = {
+  num: [4, 7],
+  rps: 0.1,
+  radius: [5, 40],
+  life: [1.5, 3],
+  v: [2, 3],
+  tha: [-50, 50],
+  alpha: [0.6, 0],
+  scale: [.1, 0.9],
+  position: "all",
+  color: ["random", "#ff0000"],
+  cross: "dead",
+  random: 10
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ParticlesBg type='custom' config={config} bg={true}/>
+        <Navigation/>
+        <Logo />
+        <Rank/>
+        <ImageLinkForm />
+      {/*    <Facerecognition /> */}
     </div>
   );
 }
