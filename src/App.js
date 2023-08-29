86c37bd7339f399eceb40e4302e478768c42a0e5
Prokop Ferdan <prokes1@gmail.com>
2023-08-29 22:12:20 +0200

    added register functionality

--- src/App.js
+++ src/App.js
@@ -70,8 +70,27 @@ class App extends Component {
       box: {},
       route: 'signin',
       isSignedIn: false,
+      user: {
+            id: '',
+            name: '',
+            email: '',
+            entries: 0,
+            joined: ''
+      }
     }
   }
+
+  loadUser = (data => {
+    this.setState({
+      user:
+      {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  })
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
@@ -130,7 +149,7 @@ class App extends Component {
         : (
           route === 'signin'
           ? <SignIn onRouteChange={this.onRouteChange}/>
-          : <Register onRouteChange={this.onRouteChange}/>
+          : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
           ) 
         }
     </div>
